25046f0372940e9be1f750ea799d87b399f60491
Artem <artem.kovalenko190203@gmail.com>
2022-05-11 18:14:54 +0300

    Fixed database path
    (cherry picked from commit ab1e996f2a9af6db0712d7485337cd48d31282df)

---
+++ app/src/main/java/com/example/dicetasks/DataBaseHelper.java
@@ -0,0 +1,103 @@
+package com.example.dicetasks;
+
+import android.content.Context;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+class DataBaseHelper extends SQLiteOpenHelper {
+    private static String DB_PATH; // полный путь к базе данных
+    private static String DB_NAME = "tasks.db";
+    private SQLiteDatabase myDataBase;
+    // названия столбцов
+    static final String COLUMN_ID = "_id";
+    static final String COLUMN_TITLE = "title";
+    static final String COLUMN_DESCRIPTION = "description";
+    static final String COLUMN_CATEGORY = "category";
+    private Context myContext;
+
+    public DataBaseHelper(Context context) {
+        super(context, DB_NAME, null, 1);
+        this.myContext = context;
+        DB_PATH =context.getFilesDir().getPath() + DB_NAME;
+    }
+
+    void createDataBase() throws IOException {
+        boolean dbExist = checkDataBase();
+
+        if (!dbExist) {
+            this.getReadableDatabase();
+            try {
+                copyDataBase();
+            } catch (IOException e) {
+                throw new Error("Error copying database");
+            }
+        }
+    }
+
+    private boolean checkDataBase(){
+        SQLiteDatabase checkDB = null;
+
+        try{
+            String myPath = DB_PATH + DB_NAME;
+            checkDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+        } catch(SQLiteException e){
+            //база еще не существует
+        }
+        if(checkDB != null){
+            checkDB.close();
+        }
+        return checkDB != null;
+    }
+
+    private void copyDataBase() throws IOException{
+        //Открываем локальную БД как входящий поток
+        InputStream myInput = myContext.getAssets().open(DB_NAME);
+
+        //Путь ко вновь созданной БД
+        String outFileName = DB_PATH + DB_NAME;
+
+        //Открываем пустую базу данных как исходящий поток
+        OutputStream myOutput = new FileOutputStream(outFileName);
+
+        //перемещаем байты из входящего файла в исходящий
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = myInput.read(buffer))>0){
+            myOutput.write(buffer, 0, length);
+        }
+
+        //закрываем потоки
+        myOutput.flush();
+        myOutput.close();
+        myInput.close();
+    }
+
+    public void openDataBase() throws SQLException {
+        //открываем БД
+        String myPath = DB_PATH + DB_NAME;
+        myDataBase = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+    }
+
+    @Override
+    public synchronized void close() {
+        if(myDataBase != null)
+            myDataBase.close();
+        super.close();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    }
+}
